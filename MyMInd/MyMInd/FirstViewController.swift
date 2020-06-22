7fa5b6f06c2f462fb2f93b9863002509a83887e1
kylefloersch <kylefloersch@yahoo.com>
2020-06-22 14:17:16 -0400

    Initial Commit. Starter Files

---
+++ MyMInd/MyMInd/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyMInd
+//
+//  Created by Kyle Floersch on 6/22/20.
+//  Copyright © 2020 Kyle Floersch. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyMInd/MyMInd/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyMInd
+//
+//  Created by Kyle Floersch on 6/22/20.
+//  Copyright © 2020 Kyle Floersch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
